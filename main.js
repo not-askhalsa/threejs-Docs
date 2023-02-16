09859e5ef762b83a37ed47bb63f3e66628c7e82f
notaskhalsa <askhalsa29@gmail.com>
2023-02-16 13:19:22 -0500

    change relative refernce for three

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import './style.css';
-import * as THREE from './node_modules/three';
+// import './style.css';
+import * as THREE from './node_modules/three/build/three';
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Setup
