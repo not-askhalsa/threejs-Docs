d4f8f5ebeeb509a08beb4233d931147cf686b454
notaskhalsa <askhalsa29@gmail.com>
2023-02-20 12:57:07 -0500

    change from vite to webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+        entry: './main.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+        },
+        devServer: {
+            static: {
+              directory: path.join(__dirname, ''),
+            },
+            compress: true,
+            port: 3000,
+        },
+    };
